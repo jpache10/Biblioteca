dd644e193faf41c5209f086aa41743f684fa4fff
dalvinxo <dalvingamer@gmail.com>
2024-06-02 18:41:29 -0400

    doc: organizacion codigo del program

--- Program.cs
+++ Program.cs
@@ -5,16 +5,23 @@ using Biblioteca.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// builder.Services.AddDbContext<SqlDatabaseBibliotecaContext>(options =>
-//     options.UseSqlServer(builder.Configuration.GetConnectionString("Biblioteca") ?? throw new InvalidOperationException("Connection string 'sDatabaseContext' not found.")));
 
+#region Agregando servicios al contenedor
+
+#region configuración de conexión de base de datos
 var connectionString = builder.Configuration.GetConnectionString("WebApiDatabase") ?? "Data Source=BibliotecaDatabase.db";
 builder.Services.AddSqlite<SqlDatabaseBibliotecaContext>(connectionString);
+#endregion
 
 
 builder.Services.AddControllersWithViews();
 
+
+#endregion
+
 var app = builder.Build();
+
+#region Agregando configuraciones 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -34,5 +41,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+#endregion
 
 app.Run();
