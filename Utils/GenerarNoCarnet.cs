ae831b750dade424836a8f1c59553663dae2f957
dalvinxo <dalvingamer@gmail.com>
2024-06-08 23:49:10 -0400

    feat: se agrego metodo para generarNoCarnet

---
+++ Utils/GenerarNoCarnet.cs
@@ -0,0 +1,23 @@
+namespace Biblioteca.Utils
+{
+
+    public static partial class GenerarNoCarnet
+    {
+
+        static string GenerarFormato()
+        {
+            Random rnd = new Random();
+            string numeros = "";
+            for (int i = 0; i < 6; i++)
+            {
+                numeros += rnd.Next(0, 10); // Genera un número aleatorio entre 0 y 9
+            }
+            int añoActual = DateTime.Now.Year;
+            return $"A{numeros}-{añoActual}";
+        }
+
+    }
+
+}
+
+
